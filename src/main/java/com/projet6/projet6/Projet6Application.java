c22b5a4a9ca59e71bb45d61db949396c74aa752d
KaiKrk <kaiqiang@outlook.fr>
2019-02-21 18:06:34 +0100

    initial commit

---
+++ src/main/java/com/projet6/projet6/Projet6Application.java
@@ -0,0 +1,13 @@
+package com.projet6.projet6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet6Application.class, args);
+	}
+
+}
